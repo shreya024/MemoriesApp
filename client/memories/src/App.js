b9562ed9cd2520d2c00ae2caabd0631455632f18
SahilDahekar <dahekar30sahil@gmail.com>
2022-08-05 23:39:19 +0530

    Added /login path

--- client/memories/src/App.js
+++ client/memories/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Container, AppBar, Typography } from "@material-ui/core";
+import Login from "./components/Login/Login"
 import Home from "./components/Home";
 import css from "./App.module.css";
 import PostDetails from "./components/PostDetails/PostDetails";
@@ -9,14 +10,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
-        <AppBar className={css.appBar} position="static" color="inherit">
-          <Typography className={css.heading} variant="h2" align="center">
-            Best Places in the World
-          </Typography>
-        </AppBar>
+        
         <Switch>
-          <Route path="/" exact component={() => <Redirect to="/posts" />} />
+          <Route path="/" exact component={() => <Redirect to="/login" />} />
           <Route path="/posts" exact component={Home} />
+          <Route path="/login" exact component={Login} />
           <Route path="/posts/search" exact component={Home} />
           <Route path="/posts/:id" component={PostDetails} />
         </Switch>
