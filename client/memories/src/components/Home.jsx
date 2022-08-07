24925accb68cc1c40f5ec9166f49c36611392935
SahilDahekar <dahekar30sahil@gmail.com>
2022-08-07 12:38:50 +0530

    updated useHistory hook to useNavigate

--- client/memories/src/components/Home.jsx
+++ client/memories/src/components/Home.jsx
@@ -7,14 +7,16 @@ import {
   AppBar,
   TextField,
   Button,
+  Typography,
 } from "@material-ui/core";
 import ChipInput from "material-ui-chip-input";
-import { useHistory, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import Posts from "./Posts/Posts";
 import Form from "./Form/Form";
 import { getPosts, getPostsBySearch } from "../actions/posts";
 import Paginate from "./Pagination";
+import css from "../App.module.css";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
@@ -25,7 +27,7 @@ const Home = () => {
   const dispatch = useDispatch();
 
   const query = useQuery();
-  const history = useHistory();
+  const history = useNavigate();
   const page = query.get("page") || 1;
   const searchQuery = query.get("searchQuery");
 
@@ -62,49 +64,56 @@ const Home = () => {
   };
 
   return (
-    <Grow in>
-      <Container maxWidth="xl">
-        <Grid
-          container
-          justifyContent="space-between"
-          alignItems="stretch"
-          spacing={3}
-        >
-          <Grid item xs={12} sm={6} md={9}>
-            <Posts setCurrentId={setCurrentId} />
+    
+      <Grow in>
+        <Container maxWidth="xl">
+          <AppBar className={css.appBar} position="static" color="inherit">
+            <Typography className={css.heading} variant="h2" align="center">
+              Best Places in the World
+            </Typography>
+          </AppBar>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={6} md={9}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <AppBar position="static" color="inherit">
+                <TextField
+                  name="search"
+                  variant="outlined"
+                  label="Search Memories"
+                  onKeyPress={handleKeyPress}
+                  fullWidth
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <ChipInput
+                  style={{ margin: "10px 0" }}
+                  value={tags}
+                  onAdd={handleAdd}
+                  onDelete={handleDelete}
+                  label="Search Tags"
+                  variant="outlined"
+                />
+                <Button onClick={searchPost} color="primary" variant="contained">
+                  Search
+                </Button>
+              </AppBar>
+              <br />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+              <Paper elevation={6}>
+                <Paginate page={page} />
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppBar position="static" color="inherit">
-              <TextField
-                name="search"
-                variant="outlined"
-                label="Search Memories"
-                onKeyPress={handleKeyPress}
-                fullWidth
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-              />
-              <ChipInput
-                style={{ margin: "10px 0" }}
-                value={tags}
-                onAdd={handleAdd}
-                onDelete={handleDelete}
-                label="Search Tags"
-                variant="outlined"
-              />
-              <Button onClick={searchPost} color="primary" variant="contained">
-                Search
-              </Button>
-            </AppBar>
-            <br />
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
-            <Paper elevation={6}>
-              <Paginate page={page} />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </Grow>
+        </Container>
+      </Grow>
+
   );
 };
 
