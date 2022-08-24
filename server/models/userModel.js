47337df7d1c0997d71f711b53ad09ef298a9f5e2
Kartik <kjmickey003@gmail.com>
2022-08-24 17:16:57 +0530

    Implement user model

---
+++ server/models/userModel.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+import bcrypt from "@node-rs/bcrypt";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true },
+    password: String,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    emailVerificationToken: String,
+    emailVerified: Boolean,
+
+    // // OAuth for future scope
+    // facebook: String,
+    // twitter: String,
+    // google: String,
+    // github: String,
+    // tokens: Array,
+
+    // posts array will store the post's ID of the user
+    posts: {
+      type: ["String"]
+    },
+
+    notifications: {
+      type: ["String"]
+    },
+
+    profile: {
+      name: String,
+      gender: String,
+      location: String,
+      website: String,
+      picture: String
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  try {
+    user.password = await bcrypt.hash(user.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to help validate passwords
+userSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword,
+  cb
+) {
+  try {
+    cb(null, await bcrypt.verify(candidatePassword, this.password));
+  } catch (err) {
+    cb(err);
+  }
+};
+
+// Method to get user's gravatar(profile picture)
+userSchema.methods.gravatar = function gravatar(size) {
+  if (!size) {
+    size = 200;
+  }
+  if (!this.email) {
+    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
+  }
+  const md5 = crypto.createHash("md5").update(this.email).digest("hex");
+  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
