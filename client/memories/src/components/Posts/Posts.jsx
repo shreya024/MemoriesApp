e57355b0b1ffc5b4e32d13ade1c779673264f638
SahilDahekar <dahekar30sahil@gmail.com>
2022-09-11 16:52:30 +0530

    Made the posts responsive

--- client/memories/src/components/Posts/Posts.jsx
+++ client/memories/src/components/Posts/Posts.jsx
@@ -12,7 +12,7 @@ const Posts = ({ setCurrentId }) => {
   ) : (
     <Grid container alignItems="stretch" spacing={3}>
       {posts.map((post) => (
-        <Grid key={post._id} item xs={12} sm={4}>
+        <Grid key={post._id} item xs={12} md={4}>
           <Post post={post} setCurrentId={setCurrentId} />
         </Grid>
       ))}
